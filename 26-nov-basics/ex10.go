1de5fbbed9de759439f78979096a6da7eacaeef7
RishabhBansal22 <aggrishabh.23@gmail.com>
2025-11-29 13:33:16 +0530

    completed ex10

--- 26-nov-basics/ex10.go
+++ 26-nov-basics/ex10.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // **Problem 10: The Payment Gateway (Interfaces)** **Concept:** Polymorphism
 
 // * Define an interface `PaymentMethod` with a single method `Pay(amount int)`.
@@ -7,3 +9,30 @@ package main
 // * Implement the `Pay` method for both (e.g., CreditCard prints "Paid $X via Card", Bitcoin prints "Paid $X via BTC").
 // * Write a function `Process(p PaymentMethod, amount int)` that takes the interface as an argument.
 // * In `main`, pass both a `CreditCard` and a `Bitcoin` instance to `Process`.
+
+type PaymentMethod interface {
+	pay(amount int)
+}
+
+type CreditCard struct{}
+
+type Bitcoin struct{}
+
+func (c CreditCard) pay(amount int) {
+	fmt.Printf("Paid %d via Card\n", amount)
+}
+
+func (b Bitcoin) pay(amount int) {
+	fmt.Printf("Paid %d via bitcoin\n", amount)
+}
+
+func Process(p PaymentMethod, amount int) {
+	p.pay(amount)
+}
+
+func main() {
+	c := CreditCard{}
+	b := Bitcoin{}
+	Process(c, 100)
+	Process(b, 150)
+}
